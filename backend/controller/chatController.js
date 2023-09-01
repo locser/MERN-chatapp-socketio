5f8f44dea8873c047ed9bb84782f504157a46a16
Pham Huu Loc <ploc280301@gmail.com>
2023-09-01 17:13:22 +0700

    test amend lần 4

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -242,6 +242,7 @@ const addToGroup = asyncHandler(async (req, res) => {
 const commit1 = {};
 const commit2_amend = {};
 const commit3_amend = {};
+const commit4_amend = {};
 
 module.exports = {
   getAllChatCurrentUser,
